1b165d22b626c9a4082d0e5938dfd986b5c15989
Raviezz <projectworkkmit@gmail.com>
2020-03-31 08:03:55 +0530

    added a program , minimin_splits

---
+++ competitive/minimum_splits.py
@@ -0,0 +1,19 @@
+"""
+> Minimum splits required to convert a number into prime segments
+> Given a number in the form of a string s, the task is to calculate and display minimum splits
+    required such that the segments formed are Prime or print Not Possible otherwise.
+
+   Examples:
+       1. Input: s = “2351”
+          Output : 0
+          Explanation: Given number is already prime.Input: s = “2351”
+
+       2.  Input: s = “2352”
+           Output: 2
+            Explanation: Resultant prime segments are 23,5,2
+
+        3.  Input: s = “2375672”
+            Output : 2
+            Explanation: Resultant prime segments are 2,37567,2
+
+"""
